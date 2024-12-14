9210016798f814401bc0460eccafb6521787f766
professorDeveloper <azamovhud007@outlook.com>
2024-12-15 02:39:40 +0500

    some admin_panel functions done

--- admin_panel/admin_data.py
+++ admin_panel/admin_data.py
@@ -1,6 +1,21 @@
 admin_info = {
     "admin_login": "superadmin",
     "admin_password": "1234",
+    'messages':[
+        {
+            "title": "Account Issue",
+            "description": "I cannot log in to my account. Please assist.",
+            "from": {
+                "id": 1,
+                "name": "John Doe",
+                "login": "john123"
+            },
+            "to": {
+                "id": "superadmin",
+                "role": "admin"
+            }
+        },
+    ],
     "role": "admin"
 }
 
