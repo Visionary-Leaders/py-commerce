6116e85466c1f2b8618adda798e02399b4807dc9
professorDeveloper <azamovhud007@outlook.com>
2024-12-15 02:38:30 +0500

    some admin_panel functions done

--- admin_panel/admin_service.py
+++ admin_panel/admin_service.py
@@ -5,7 +5,11 @@ from user_panel.user_data import users, checkUser, getUserId
 from user_panel.user_service import userPage
 from datetime import datetime
 
-products = []
+products = [
+    {"id": 1, "product_name": "Laptop", "product_price": "1000", "product_date": "12/15/2024 10:30:45"},
+    {"id": 2, "product_name": "Smartphone", "product_price": "500", "product_date": "12/16/2024 11:25:32"},
+    {"id": 3, "product_name": "Tablet", "product_price": "300", "product_date": "12/17/2024 12:40:50"}
+]
 
 
 # ######################### Admin Page ####################
@@ -33,14 +37,119 @@ def addProduct():
         "product_price": productPrice,
         "product_date": productDate
     })
-    display_loading_animation(lang['loading'],Color.CYAN) # Dizayn uchun
+    display_loading_animation(lang['loading'], Color.CYAN)  # Dizayn uchun
     println_colored(lang['success_added_product'], Color.GREEN)
 
 
-
 def editProduct():
+    println_colored("========================== ProductList ==========================", Color.CYAN)
+    for product in products:
+        println_colored(
+            f"ID: {product['id']} | {lang['name']}: {product['product_name']} | {lang['price']}: {product['product_price']} | {lang['date']}: {product['product_date']}",
+            Color.DARK_ORANGE)
+    println_colored("============================================================", Color.CYAN)
+
+    product_id = int(input(lang['enter_product_id'] + ": "))
+    selected_product = next((product for product in products if product['id'] == product_id), None)
+
+    if selected_product:
+        new_name = input(lang['edit_product_name'] + f" (current: {selected_product['product_name']}): ")
+        new_price = input(lang['edit_product_price'] + f" (current: {selected_product['product_price']}): ")
+
+        if new_name.strip():
+            selected_product['product_name'] = new_name
+        if new_price.strip():
+            selected_product['product_price'] = new_price
+
+        current_time = datetime.now()
+        selected_product['product_date'] = current_time.strftime("%m/%d/%Y %H:%M:%S")
+
+        display_loading_animation(lang['loading'], Color.CYAN)
+        println_colored(lang['success_edited_product'], Color.GREEN)
+    else:
+        println_colored(lang['product_not_found'], Color.RED)
+
+
+def maxPriceProduct():
+    if len(products) == 0:
+        println_colored(lang['no_products'], Color.RED)
+        return
+
+    most_sold = max(products, key=lambda product: float(product['product_price']))
+
+    println_colored(
+        f"ID: {most_sold['id']} | Name: {most_sold['product_name']} | Price: {most_sold['product_price']} | Date: {most_sold['product_date']}",
+        Color.CYAN)
+
+
+def mostSoldProduct():
     pass
 
+
+def searchProduct():
+    search_term = input(f"{lang['search_product_name']} : ").lower()
+    found = False
+
+    for product in products:
+        if search_term in product['product_name'].lower():
+            println_colored(
+                f"ID: {product['id']} | {lang['name']}: {product['product_name']} | {lang['price']}: {product['product_price']} | {lang['date']}: {product['product_date']}",
+                Color.DARK_ORANGE)
+            found = True
+
+    if not found:
+        println_colored(lang['product_not_found'], Color.RED)
+
+
+def messageToUser():
+    println_colored("==================== Users ====================", Color.BLUE)
+    for user in users:
+        println_colored(f"ID: {user['id']} | {lang['name']}: {user['name']} | {lang['login']}: {user['login']}",
+                        Color.CYAN)
+    println_colored("===============================================", Color.BLUE)
+
+    recipient_id = int(input(f"{lang['enter_recipent_id']} "))
+
+    recipient = None
+    for user in users:
+        if user['id'] == recipient_id:
+            recipient = user
+            break
+
+    if recipient:
+
+        title = input(f"{lang['enter_message_title']}: ")
+        message_description = input(f"{lang['enter_message_description']}: ")
+
+        message = {
+            "title": title,
+            "description": message_description,
+            "from": {"id": 0, "name": "SuperAdmin", "login": "super_admin"},  # Sender is the Admin
+            "to": recipient  # Recipient user details
+        }
+
+        if 'messages' not in recipient:
+            recipient['messages'] = []
+        recipient['messages'].append(message)
+
+        println_colored(f"{recipient['name']} {lang['message_sent_to']} !", Color.GREEN)
+    else:
+        println_colored(lang['user_not_found'], Color.RED)
+
+
+def messageToMe():
+    print(f"\n===== {lang['admin_inbox']} =====")
+
+    if not admin_info['messages']:
+        println_colored(f"{lang['no_message']}", Color.RED)
+        return
+
+    for message in admin_info['messages']:
+        print("==================================")
+        println_colored(f"{lang['message_from']}  {message['from']['name']} (ID: {message['from']['id']})", Color.CYAN)
+        println_colored(f"{lang['enter_message_title']} {message['title']}", Color.CYAN)
+        println_colored(f"{lang['enter_message_description']} {message['description']}", Color.CYAN)
+        print("==================================")
 
 def admin_page():
     while True:
@@ -50,12 +159,13 @@ def admin_page():
         println_colored(f"3 -> {lang['most_sold_product']}", Color.MAGENTA)
         println_colored(f"4 -> {lang['my_shop_balance']}", Color.MAGENTA)
         println_colored(f"5 -> {lang['search_product']}", Color.MAGENTA)
-        println_colored(f"6 -> {lang['all_product_price']}", Color.MAGENTA)
-        println_colored(f"7 -> {lang['message_with_user']}", Color.MAGENTA)
+        println_colored(f"6 -> {lang['message_to_user']}", Color.MAGENTA)
+        println_colored(f"7 -> {lang['message_to_me']}", Color.MAGENTA)
         println_colored(f"8 -> {lang['user_list']}", Color.MAGENTA)
         println_colored(f"9 -> {lang['most_buyer_user']}", Color.MAGENTA)
         println_colored(f"10 -> {lang['remove_product']}", Color.MAGENTA)
-        println_colored(f"11 -> {lang['log_out']}", Color.MAGENTA)
+        println_colored(f"11 -> {lang['max_price_product']}", Color.MAGENTA)
+        println_colored(f"12 -> {lang['log_out']}", Color.MAGENTA)
         println_colored("==================================", Color.DARK_ORANGE)
         choose = int(input(lang['choice']))
         if choose == 1:
@@ -63,22 +173,24 @@ def admin_page():
         elif choose == 2:
             editProduct()
         elif choose == 3:
-            pass
+            mostSoldProduct()
         elif choose == 4:
             pass
         elif choose == 5:
-            pass
+            searchProduct()
         elif choose == 6:
-            pass
+            messageToUser()
         elif choose == 7:
-            pass
+            messageToMe()
         elif choose == 8:
             pass
         elif choose == 9:
             pass
         elif choose == 10:
             pass
-        elif choose == 1:
+        elif choose == 11:
+            maxPriceProduct()
+        elif choose == 12:
             pass
         else:
             println_colored(lang["invalid_choice"], Color.RED)
--- user_panel/user_data.py
+++ user_panel/user_data.py
@@ -1,4 +1,15 @@
-users = []
+users = [
+    {
+        "id": 1,
+        "name": "John Doe",
+        "login": "john123",
+        "password": "johnme",
+        "messages": [],
+        "balance": 0,  # No balance is needed for this example
+        "myProducts": [],  # User's own products
+        "favoriteProducts": []  # User's favorite products
+    },
+]
 
 
 def checkUser(login, password):
